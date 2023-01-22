e24fcc7fbea920c79fdc404c898e562301f56b24
Mohd-muneer <mohdmuneeruddin456@gmail.com>
2023-01-23 00:09:14 +0530

    used {router,route,routes,link} for linking

--- feedback-app/src/components/RatingSelect.jsx
+++ feedback-app/src/components/RatingSelect.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 
 function RatingSelect({select}) {
-    const [selected,setSelected]=useState(10)
+    const [selected,setSelected]=useState('')
 
     const handleChange= (e)=>{
         setSelected(+e.currentTarget.value);
